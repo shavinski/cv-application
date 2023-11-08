52995bb3aa29d3cf277b0bfd23250d398091ab81
Jakob <shavinski.jakob@gmail.com>
2023-11-08 15:11:06 -0800

    Add a close button to modal form for skills

--- src/components/SkillsForm.tsx
+++ src/components/SkillsForm.tsx
@@ -73,6 +73,11 @@ function SkillsForm({ handleSkillsInfoChange, skills }: SkillsFormProps) {
                     <div className="modal">
                         <div onClick={displayModalForm} className="overlay"></div>
                         <form className="modal-form-skills">
+                            <button type="button" className="modal-close-btn" onClick={displayModalForm}>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-lg" viewBox="0 0 16 16">
+                                    <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z" />
+                                </svg>
+                            </button>
                             <label htmlFor="category">What category would you like to add to?</label>
                             <select id="category" name="category" onChange={handleCategoryChange}>
                                 {Object.keys(skills).map((cat) => (
