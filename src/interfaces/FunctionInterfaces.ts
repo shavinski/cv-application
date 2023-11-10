00a32e0398ae3945726fda7fdfb14c952a7c79de
Jakob <shavinski.jakob@gmail.com>
2023-11-10 13:25:42 -0800

    Move frontend code into frontend directory
    Will add a backend later to handle creating a google document for user to link to google drive

--- src/interfaces/FunctionInterfaces.ts
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export interface ChangeInfoHandler {
-    (e: React.ChangeEvent<HTMLInputElement>): void;
-}
-
-export interface ChangeSkillsHandler {
-    (skillsCategory: string, newSkill: string[]): void;
-}
